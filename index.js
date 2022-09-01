e200c756292788d74bb697cd6b5eb41a2f606e95
Janice Teguh <janicejarusdy@gmail.com>
2022-09-01 20:31:46 +1000

    commit tree 89949d0ec2b7ec36e1d074fc3f3978ed762e0b3f

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(number1, number2){
+    return number1 + number2
+}
+
+function subtract(number1, number2) {
+    return number1 - number2
+}
+
+function multiply(a, b) {
+    return a*b
+}
+
+function divide(a, b) {
+    return a/b
+}
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
